85d59953bc96a9d0fade4f94cedd5ca5807b673b
Zhixun LIN (aka viogus) <zhixun.lin@gmail.com>
2010-04-27 16:07:52 +0800

    zenburn theme rules all.

--- config/look.el
+++ config/look.el
@@ -1,5 +1,5 @@
 ;; -*- mode: Emacs-Lisp -*-
-;; Time-stamp: <2010-04-02 03:47:33 +800 Zhixun LIN>
+;; Time-stamp: <2010-04-27 16:06:58 +800 Zhixun LIN>
 ;;关闭起动时的那个“开机画面”。 
 (setq inhibit-startup-message t)
 (setq initial-scratch-message nil)
@@ -16,34 +16,14 @@
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
-(cond ((not window-system)
-       ;; 如果不在 window 环境中(字符界面时)
-       (setq frame-background-mode 'dark)
-       ;; 设置背景为黑的，这样 Emacs 的很多缺省颜色会和黑色背景协调。
-       (eval-after-load "log-view"
-         ;; 设置 log-view 的颜色。
-         '(progn
-            (set-face-attribute 'log-view-file-face nil :foreground "blue" :weight 'bold)
-            (set-face-attribute 'log-view-message-face nil :foreground "yellow" :weight 'bold)))
 
-      ((eq window-system 'x)
-       ;; 如果在 X Window 中
-       ;;       (global-set-key (kbd "C--") 'undo)
-       ;; Console 中习惯了 C-_ 作为 Undo，不妨把这个也改过来。
+;; X Window 中的 visible-bell 还是很好看的。
+(setq visible-bell t)
+;;(color-theme-initialize)
+;; One theme rules all.
+(color-theme-zenburn)
 
-       (setq visible-bell t)
-       ;; X Window 中的 visible-bell 还是很好看的。
-
-       (setq x-stretch-cursor nil)
-       ;; 如果设置为 t，光标在 TAB 字符上会显示为一个大方块 :)。
-
-       (scroll-bar-mode -1)
-       (tool-bar-mode -1)
-       (menu-bar-mode -1)
-       ;; 不要 tool-bar 和 scroll-bar。
-
-       (require 'tmtheme)
-       (setq tmtheme-directory "~/.emacs.d/tmthemes")
-       (tmtheme-scan)
-       (tmtheme-Blackboard)
-       )))
+;(require 'tmtheme)
+;(setq tmtheme-directory "~/.emacs.d/tmthemes")
+;(tmtheme-scan)
+;(tmtheme-Blackboard)
